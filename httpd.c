3f1edb34ba8e22b6cc53b7048eeb0f12fc067b07
kcarri14 <kcarri14@calpoly.edu>
2024-11-21 12:54:34 -0800

    aghhhhh

--- httpd.c
+++ httpd.c
@@ -86,17 +86,18 @@ void handle_request(int nfd)
       fstat(file, &file_stat);
 
       char header[512];
-      if(strcmp(type, "GET") == 0 || strcmp(type, "HEAD") == 0){
+      if(strcmp(type, "HEAD") == 0){
          snprintf(header, sizeof(header), "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\nContent-Length: %ld\r\n\r\n", file_stat.st_size);
          write(nfd, header, strlen(header));
 
-         if(strcmp(type, "GET") == 0){
+         
+     }else if(strcmp(type, "GET") == 0){
             char buffer[1024];
             ssize_t read_bytes;
             while((read_bytes = read(file, buffer,sizeof(buffer)))> 0){
                write(nfd, buffer, read_bytes);
             }
-         }
+         
      }
      close(file);
    }
